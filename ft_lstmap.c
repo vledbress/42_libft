b0c9c81e7f98cc3bd792297530a406b33e51f981
Vladyslav Borysov <vborysov@c1r1s6.42prague.com>
2025-11-07 15:28:07 +0100

    libft

---
+++ ft_lstmap.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vborysov <vborysov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 15:22:12 by vborysov          #+#    #+#             */
+/*   Updated: 2025/11/07 15:25:28 by vborysov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*result;
+	t_list	*current;
+
+	if (!lst)
+		return (NULL);
+	while (lst)
+	{
+		current = ft_lstnew(f(lst->content));
+		if (!current)
+		{
+			ft_lstclear(&result, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&result, current);
+		lst = lst->next;
+	}
+	return (result);
+}
